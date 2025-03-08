940cce7319393a11a147e9683abe4ff0470be976
Rita Vasques <ritavasques@iMac.home>
2025-03-08 18:33:38 +0100

    ex02

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ritavasques <ritavasques@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 13:13:13 by ritavasques       #+#    #+#             */
+/*   Updated: 2025/03/08 18:16:36 by ritavasques      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(void) : AForm("", 72, 45), _target("") {
+    std::cout << "Robotomy Default constructor" << std::cout;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target) : AForm("Robotomy", 72, 45), _target(target) {
+    std::cout << "Robotomy constructor" << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm & other) : AForm(other), _target(other._target) {
+    std::cout << "Robotomy copy constructor" << std::endl;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(void) {
+    std::cout << "Robotomy destructor" << std::endl;
+}
+		
+RobotomyRequestForm & RobotomyRequestForm::operator=(const RobotomyRequestForm & other) {
+    std::cout << "Robotomy assignment operator" << std::endl;
+    this->_target = other._target;
+    return *this;
+}
+		
+void RobotomyRequestForm::execute(Bureaucrat const & executor) const {
+    if (this->getSigned() == false)
+        throw FormNotSignedException();
+	if (executor.getGrade() > this->getGradeExc())
+        throw GradeTooLowException();
+
+	std::cout << executor.getName() << " executed " << this->getName() << std::endl;
+    
+    std::cout << "*LOUD DRILLING NOISES*" << std::endl;
+    srand(time(0));
+    if (rand() % 2 == 0)
+        std::cout << this->_target << " has been robotomized successfully" << std::endl;
+    else
+        std::cout << this->_target << " robotomization failed" << std::endl;
+}
